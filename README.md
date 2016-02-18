6af499b6b8b4829eb0bcde7d78fecb7f7ef2e17d
manglasape <100303534@alumnos.uc3m.es>
2016-02-19 00:35:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dicksbot
 Telegram inline bot that sends NSFW texts.
+In my defense, I'll say that this was an experiment C:
---
+++ t.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+require 'telegram/bot'
+require 'json'
+require 'net/http'
+token = '145094166:AAGDJgVnujnghM9zPnAxGCuLJQi93gIPOK8'
+
+def getRekt(uri)
+  url = URI.parse(uri)
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+  }
+  @ret = res
+end
+
+def getRandom()
+  get = getRekt('http://dicks-api.herokuapp.com/dicks/1')
+  data = JSON.parse(get.body)
+  @ret = data['dicks'][0]
+end
+
+
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+
+    begin
+      case message
+      when Telegram::Bot::Types::InlineQuery
+        results = [
+          Telegram::Bot::Types::InlineQueryResultArticle
+            .new(id: 1, title: 'Cute', message_text: '8===D'),
+          Telegram::Bot::Types::InlineQueryResultArticle
+            .new(id: 2, title: 'Average', message_text: '8=====D'),
+          Telegram::Bot::Types::InlineQueryResultArticle
+            .new(id: 3, title: 'XL', message_text: '8==============D'),
+          Telegram::Bot::Types::InlineQueryResultArticle
+            .new(id: 4, title: 'Random', message_text: getRandom)
+        ]
+        bot.api.answer_inline_query(inline_query_id: message.id, results: results)
+      when Telegram::Bot::Types::Message
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+      end
+    rescue Telegram::Bot::Exceptions::ResponseError => e
+        puts e.message
+    end
+  end
+end
